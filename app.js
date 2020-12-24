c0ea76fc5931aafebed137b5bc5c91d655332d8e
clhettiarachchi <chandikalakshan@gmail.com>
2020-12-24 07:33:48 +0530

    update app port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const blogRoutes = require("./routes/blogRoutes")
 // connect to mongodb
 const dbURI = 'mongodb+srv://chandika:chandika1994@nodetuts.eh9zl.mongodb.net/<node-tuts>?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(3000))
+.then((result) => app.listen())
 .catch((err) => console.log(err));
 
 // register view engine
